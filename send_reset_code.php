bde243806fb999ce9acf5c16883cb86f6306f671
kaouter339 <289433110+kaouter339@users.noreply.github.com>
2026-06-03 14:57:50 +0200

    Update send_reset_code.php

--- send_reset_code.php
+++ send_reset_code.php
@@ -61,8 +61,8 @@ $mail = new PHPMailer();
 $mail->isSMTP();
 $mail->Host = "smtp.gmail.com";
 $mail->SMTPAuth = true;
-$mail->Username = "maktabatisaghira4@gmail.com";
-$mail->Password = "zgwcmjaylbaqjqgv";
+$mail->Username = "";
+$mail->Password = "";
 $mail->SMTPSecure = "tls";
 $mail->Port = 587;
 
