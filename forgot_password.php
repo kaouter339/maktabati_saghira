7fc2e587013e7dacf59972883ce8eb5263122318
kaouter339 <289433110+kaouter339@users.noreply.github.com>
2026-08-11 08:35:28 +0200

    Update forgot_password.php

--- forgot_password.php
+++ forgot_password.php
@@ -33,9 +33,9 @@ $mail->isSMTP();
 $mail->Host = "smtp.gmail.com";
 $mail->SMTPAuth = true;
 
-$mail->Username = "maktabatisaghira4@gmail.com";
+$mail->Username = "";
 
-$mail->Password = "zgwcmjaylbaqjqgv";
+$mail->Password = "";
 
 $mail->SMTPSecure = "tls";
 $mail->Port = 587;
